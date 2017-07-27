e5ee75065a82a6774bfcb6c0c869aa5e5cd45173
BFAlex <838318741@qq.com>
2017-07-27 08:11:43 +0800

    Initial Commit

---
+++ BFScrollViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFScrollViewDemo
+//
+//  Created by Readboy_BFAlex on 2017/7/27.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
